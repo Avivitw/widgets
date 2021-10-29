d64fd2ee3cb37ef76619e0e5f161fee4edc6e69a
Avivit Weissman <wavivit@gmail.com>
2021-10-29 12:47:05 -0700

    Added axios to project and add api request to wiki

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  useEffect(() => {
+    const search = async () => {
+      await axios.get("https://en.wikipedia.org/w/api.php?", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>
@@ -9,6 +25,7 @@ const Search = () => {
         <div className="field">
           <label>Enter Search Term</label>
           <input
+            value={term}
             className="input"
             onChange={(e) => setTerm(e.target.value)}
           ></input>
