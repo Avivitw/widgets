462bc8dadf95bc030517bca25c7869b860de41e4
Avivit Weissman <wavivit@gmail.com>
2021-11-01 16:05:19 -0700

    refactor the search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,19 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("programing");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    // cleanup timer
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -13,28 +25,13 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-
-    //if there is an initialized term we want to search immediaatly
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-
-      //cleanup func to cancel previous timer
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
